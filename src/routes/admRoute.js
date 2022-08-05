f3ea98be83a922c257d9e73093d6daabef8889ab
Lucas Rocha <lluc.rocha90@gmail.com>
2022-08-04 23:12:11 -0300

    finalizado CRUD painel adm

--- src/routes/admRoute.js
+++ src/routes/admRoute.js
@@ -10,8 +10,18 @@ router.get("/product-adm", AdmController.adm);
 router.get("/product-adm/product-create", AdmController.createProduct);
 // Rota para armazenar criação de um produto
 router.post("/product-adm/product-create", AdmController.store);
+
 // Rota para visualizar um produto
 router.get("/product-adm/:id", AdmController.viewProduct);
 
+// Rota para visualizar formulario edição
+router.get("/product-adm/product-edit/:id", AdmController.edit);
+// Rota para a edição do produto
+router.put("/product-adm/product-edit/:id", AdmController.update);
+
+// Rota para visualizar produto para excluir
+router.get("/product-adm/product-delete/:id", AdmController.delete);
+// Rota para excluir um produto
+router.delete("/product-adm/product-delete/:id", AdmController.destroy);
 
 module.exports = router;
