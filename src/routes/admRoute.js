f02101b72d7f80517173da8fd4c7cf06ee1b02ff
Rodrigo Sousa <rodrigo.sdeveloper@gmail.com>
2022-08-05 09:59:22 -0300

    Ajustes do painel administrador - Feito pelo Lucas

--- src/routes/admRoute.js
+++ src/routes/admRoute.js
@@ -2,7 +2,26 @@ const express = require("express");
 const router = express.Router();
 const AdmController = require("../controllers/AdmController");
 
+router.get("/home-adm", AdmController.homeAdm);
 router.get("/login-adm", AdmController.login);
 router.get("/product-adm", AdmController.adm);
+
+// Rota para criar um produto
+router.get("/product-adm/product-create", AdmController.createProduct);
+// Rota para armazenar criação de um produto
+router.post("/product-adm/product-create", AdmController.store);
+
+// Rota para visualizar um produto
+router.get("/product-adm/:id", AdmController.viewProduct);
+
+// Rota para visualizar formulario edição
+router.get("/product-adm/product-edit/:id", AdmController.edit);
+// Rota para a edição do produto
+router.put("/product-adm/product-edit/:id", AdmController.update);
+
+// Rota para visualizar produto para excluir
+router.get("/product-adm/product-delete/:id", AdmController.delete);
+// Rota para excluir um produto
+router.delete("/product-adm/product-delete/:id", AdmController.destroy);
 
 module.exports = router;
