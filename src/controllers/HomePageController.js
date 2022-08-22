b08a3b294661f9bb0654acfe9098c7bba41ab38f
determinedalways <juninho_zimba-net@hotmail.com>
2022-08-22 19:53:51 -0300

    deixei dinamico os elementos

--- src/controllers/HomePageController.js
+++ src/controllers/HomePageController.js
@@ -1,19 +1,20 @@
 
 const fs = require("fs");
 const path = require("path");
+const productsJson = fs.readFileSync(path.join(__dirname, "..", "data", "products.json"), "utf-8")
+const products = JSON.parse(productsJson);
 
 
 const HomePageController = {
     index: (req, res) => {
-        return res.render("home-page", {title: "Home", user: req.cookies.user, admin: req.cookies.admin});
+        return res.render("home-page", {title: "Home", user: req.cookies.user, admin: req.cookies.admin, products:products});
     },
     show: (req,res) => {
-        const productsJson = fs.readFileSync(path.join(__dirname, "..", "data", "products.json"), "utf-8")
-        const products = JSON.parse(productsJson);
+      
         const {id} = req.params
         for (let i = 0; i < products.length; i++) {
             if (products[i].id === parseInt(id)) {
-                return res.render('brand-detail-asus-detail')
+                // return res.render("products[i].url-img")
             }
             else {
                 res.status(404).json({message:"não encontrado"})
