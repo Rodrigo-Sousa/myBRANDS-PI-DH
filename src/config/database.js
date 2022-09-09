fabb7b591852c3114073e401efe5fdb6560eb2b1
Rodrigo Sousa <rodrigo.sdeveloper@gmail.com>
2022-09-08 21:30:43 -0300

    Adicionado o conteúdo correto do banco

--- src/config/database.js
+++ src/config/database.js
@@ -1,7 +1,7 @@
 require("dotenv").config();
 const database = {
   username: process.env.DB_USERNAME || "root",
-  password: process.env.DB_PASSWORD || "",
+  password: process.env.DB_PASSWORD || "MySecurity_13&*#",
   database: process.env.DB_DATABASE || "ecommerce_mybrands_db",
   host: process.env.DB_HOST || "localhost",
   dialect: process.env.DB_DIALECT || "mysql",
